02a1acaad527ddcf21151d40abd553b29d6c9d86
Angeline <angeline07561@gmail.com>
2016-03-20 10:25:39 +1100

    Finalise uber and map api

--- app/assets/javascripts/googlemap.js
+++ app/assets/javascripts/googlemap.js
@@ -1,81 +1,57 @@
 
 function initMap(event) {
   console.log(event);
-  if ((event.lat == null) || (event.long == null)) {
-    return 0;
-  }
-  console.log(event);
-  var handler = Gmaps.build('Google');
-  handler.buildMap({ internal: {id: 'map'} }, function(){
-    markers = handler.addMarkers([    // put marker method
-      {
-        "lat": event.lat,
-        "lng": event.long,
-        "picture": {
-          "url": 'map_icon.ico',
-          "width":  32,
-          "height": 32
-        },
-        "infowindow": "<b>" + event.name + ": " + event.place + ". "
-      }
-    ]);
-    handler.bounds.extendWith(markers);
-    handler.fitMapToBounds();
-    handler.getMap().setZoom(12);
+  // var temp = event["attributes"];
+  // console.log(temp);
+  var latitude = event.lat;
+  var longitude = event.long;
+  var myLatLng = {lat: latitude, lng: longitude};
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 12,
+    center: myLatLng
   });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: "<p>Find us here!</p>"
+  });
+
+  var marker = new google.maps.Marker({
+    position: myLatLng,
+    map: map,
+    title: 'Find me here',
+    animation: google.maps.Animation.DROP,
+  });
+  infowindow.open(map, marker);
+  marker.setAnimation(google.maps.Animation.BOUNCE);
+
+
+  // var handler = Gmaps.build('Google', null);
+  // handler.buildMap({ internal: {id: 'map'} }, function(){
+  //   var latitude = event.lat;
+  //   var longitude = event.long;
+  //   if ((lat == null) || (lng == null)) {
+  //     lat = -37.814509
+  //     lng = 144.963289
+  //   }
+  //   console.log(lat, lng);
+  //   markers = handler.addMarkers([    // put marker method
+  //     {
+  //       "lat": lat,
+  //       "lng": lng,
+  //       "picture": {
+  //         "url": 'map_icon.ico',
+  //         "width":  32,
+  //         "height": 32
+  //       },
+  //       "infowindow": "<b>" + temp.name + ": " + temp.place + ". "
+  //     }
+  //   ]);
+  //   handler.bounds.extendWith(markers);
+  //   handler.fitMapToBounds();
+  //   handler.getMap().setZoom(12);
+  // });
 }
-// google.maps.event.addDomListener(window, "load", initialize);
-
-// function gmap_show(event) {
-//   if ((event.lat == null) || (event.lng == null)) {
-//     return 0;
-//   }
-//   handler = Gmaps.build('Google');
-//   handler.buildMap({ provider: {}, internal: {id: 'map'}}, function(){
-//     markers = handler.addMarkers([    // put marker method
-//       {
-//         "lat": company.lat,
-//         "lng": company.lng,
-//         "picture": {
-//           "url": 'map_icon.ico',
-//           "width":  32,
-//           "height": 32
-//         },
-//         "infowindow": "<b>" + event.name + ": " + event.place + ". "
-//       }
-//     ]);
-//     handler.bounds.extendWith(markers);
-//     handler.fitMapToBounds();
-//     handler.getMap().setZoom(12);    // set the default zoom of the map
-//   });
-// }
-
-
-// function initialize(event) {
-//   var handler = Gmaps.build('Google');
-//   handler.buildMap({ internal: {id: 'map'} }, function(){
-//     if ((event.lat == null) || (event.long == null)) {
-//       return 0;
-//     }
-//     console.log(event)
-//     var marker = handler.addMarkers([
-//         {
-//           "lat": event.lat,
-//           "lng": event.long,
-//           "picture": {
-//             "url": 'map_icon.ico',
-//             "width":  32,
-//             "height": 32
-//           },
-//           "infowindow": "<b>" + event.name + ": " + event.place + ". "
-//         }
-//     ]);
-//     handler.bounds.extendWith(markers);
-//     handler.fitMapToBounds();
-//     handler.getMap().setZoom(12);
-//   });
-// }
-
 
 
 
